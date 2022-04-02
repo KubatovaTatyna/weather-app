980d02758391eb00a51ff63c272bb40aa6c9185a
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-04-03 01:14:07 +0600

    Add background video with weahter description

--- script.js
+++ script.js
@@ -5,31 +5,58 @@ let descrip = document.getElementById("descrip");
 let input = document.getElementById("input");
 let form = document.getElementById("form");
 let fail = document.getElementById("fail");
+let video = document.querySelector("video");
+let language = document.getElementById("language");
+
+video.src = './assets/extreme.mp4'
+
+// const lan = ['en', 'ru'];
+// language.addEventListener('click', () => {
+//   if(language.innerText = 'English') {
+//     language.innerText = 'Russian';
+//   }
+// })
 
 function convert(val) {
   return (val - 273).toFixed();
 }
 
 function getApi() {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=731ebe35142a1bfcacb3789bb14c5069`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=731ebe35142a1bfcacb3789bb14c5069&`)
     .then(response => response.json())
     .then(data => {
+      fail.innerHTML = '';
       input.value = '';
+
       city.innerHTML = data['name'];
       country.innerHTML = data['sys']['country'];
       temp.innerHTML = convert(data['main']['temp']) + '\xB0C / ' + (data['main']['temp']).toFixed() + "\xB0F";
       descrip.innerHTML = data['weather'][0].description;
-      console.log(data)
+
+      if(data['weather'][0].main === 'Clear') {
+        video.src = './assets/clear.mov';
+      }
+      else if(data['weather'][0].main === 'Snow') {
+        video.src = './assets/snow.mp4';
+      }
+      else if(data['weather'][0].main === 'Rain') {
+        video.src = './assets/rain.mp4';
+      }
+      else if(data['weather'][0].main === 'Clouds') {
+        video.src = './assets/clouds.mov';
+      }
+      else if(data['weather'][0].main === 'Extreme') {
+        video.src = './assets/extreme.mp4';
+      }
     })
-    .catch(event => {
+    .catch(() => {
       fail.innerHTML = "this city is not find";
       city.innerHTML = " ";
-      event.preventDefault();
-    })
+    });
 }
 
 
 form.addEventListener('submit', (event) => {
   getApi();
   event.preventDefault();
-})
+});
